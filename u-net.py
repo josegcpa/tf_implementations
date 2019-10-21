9f781e5ed17c26dcebb4748ec2a8d04b255411b9
josegcpa <josegcpa@ebi.ac.uk>
2019-10-21 15:11:41 +0100

    included tumble prediction and testing in u-net

--- u-net.py
+++ u-net.py
@@ -748,6 +748,7 @@ def main(mode,
                         for i in range(prediction.shape[0]):
                             image = prediction[i,:,:]
                             image_name = im_names[i].decode().split(os.sep)[-1]
+                            print(image_name)
                             image_name = image_name.split('.')[0]
                             image_name = image_name + '.tif'
                             image_output = os.path.join(prediction_output,
