d1d14bb6a2bea66ecfc8715786a5ca83fa2be900
josegcpa <josegcpa@ebi.ac.uk>
2020-03-12 00:25:38 +0000

    continuing work on 3 class u-net

--- u-net.py
+++ u-net.py
@@ -366,7 +366,7 @@ def main(mode,
             tf.log(tmp + 1e-8) * truth,
             (1 - truth) * tf.log(1 - tmp + 1e-8)
             ) * class_balancing
-        loss = tf.reduce_mean(loss,axis=-1)
+        loss = - tf.reduce_mean(loss,axis=-1)
 
         loss = loss * weights
         loss = tf.reduce_sum(loss)
