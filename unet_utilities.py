64efa9b2abba210ba076e42e7dce2c15be49748a
josegcpa <josegcpa@ebi.ac.uk>
2020-08-27 13:06:22 +0100

    added data generator and updated u-net

--- unet_utilities.py
+++ unet_utilities.py
@@ -1024,7 +1024,7 @@ def generate_images_h5py_dataset(h5py_path,
             random_key = np.random.choice(key_list)
             rr = segmentation_dataset[random_key]
             mask = np.concatenate([rr['mask'],1-rr['mask']],axis=2)
-            yield rr['image'],rr['mask'],rr['weight_map']
+            yield rr['image'],mask,rr['weight_map']
 
 def classification_generator(image_path_list,classification_list,
                              chances = [0,0,0],
