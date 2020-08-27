77ead8a485d274117e6c1eeb13d7ce2c7dd94d52
josegcpa <josegcpa@ebi.ac.uk>
2020-08-27 14:55:35 +0100

    added data generator and updated u-net

--- segmentation_to_h5py.py
+++ segmentation_to_h5py.py
@@ -2,6 +2,103 @@ import h5py
 import sys
 from glob import glob
 import os
+import numpy as np
+import cv2
+from math import inf
+from skimage import io
+from scipy.spatial import distance
+
+def read_image(path):
+    return io.imread(path)
+
+def get_weights(truth_image,w0=0.5,sigma=10):
+    """
+    Has to be one channel only.
+
+    Arguments [default]:
+    * truth_image - numpy array with ground truth image
+    * w0 - w0 value for the weight map [10]
+    * sigma - sigma value for the weight map [5]
+    """
+    stride = 128
+    size = 256
+    sh = truth_image.shape
+    max_x = sh[0] // stride
+    max_y = sh[1] // stride
+    if max_x % stride != 0: max_x += 1
+    if max_y % stride != 0: max_y += 1
+    kernel = np.ones((3,3))
+    truth_image = truth_image.copy()
+    truth_image[truth_image > 0] = 255
+    truth_image = cv2.morphologyEx(truth_image.astype('uint8'),
+                                   cv2.MORPH_OPEN, kernel)
+    truth_image_ = truth_image.copy()
+    edges = cv2.Canny(truth_image,100,200)
+
+    if 255 in truth_image:
+        final_weight_mask = np.zeros((sh[0],sh[1]),dtype = 'float32')
+        for i in range(max_x):
+            for j in range(max_y):
+                m_x,M_x = i*stride,i*stride + size
+                m_y,M_y = j*stride,j*stride + size
+                sub_image = truth_image[m_x:M_x,m_y:M_y]
+                sub_edges = edges[m_x:M_x,m_y:M_y]
+                tmp_weight_mask = final_weight_mask[m_x:M_x,m_y:M_y]
+
+                ssh = sub_image.shape
+                pixel_coords = np.where(sub_image == 0)
+                pixel_coords_t = np.transpose(pixel_coords)
+                weight_mask = np.zeros((ssh[0],ssh[1]),dtype = 'float32')
+
+                cp_coords = np.transpose(np.where(sub_edges > 0))
+                distances = distance.cdist(pixel_coords_t,cp_coords)
+                distances[distances == 0] = inf
+                if distances.any():
+                    mins = np.array(np.min(distances,axis = 1))
+                    weight_map = ((mins * 2) ** 2)/(2 * sigma ** 2)
+                    weight_map = w0 + ((1 - w0) * np.exp(-weight_map))
+
+                    weight_mask[pixel_coords[0],
+                                pixel_coords[1]] = weight_map
+
+                else:
+                    weight_mask = np.ones((ssh[0],ssh[1]),
+                                          dtype = 'float32') * w0
+
+                final_weight_mask[m_x:M_x,m_y:M_y] = np.where(
+                    weight_mask > tmp_weight_mask,
+                    weight_mask,
+                    tmp_weight_mask
+                )
+
+    else:
+        final_weight_mask = np.ones(truth_image.shape) * w0
+
+    final_weight_mask[truth_image > 0] = 1
+    return final_weight_mask
+
+def rotate_coords(x,y,angle):
+    angle = np.radians(angle)
+    new_x = x*np.cos(angle) - y*np.sin(angle)
+    new_y = y*np.cos(angle) + x*np.sin(angle)
+    return new_x,new_y
+
+def edges_to_polygon(edge_x,edge_y,polygon_side):
+    angles = np.arctan2(edge_y,edge_x) + np.pi
+
+    ls = np.linspace(0,2*np.pi,
+                     num=polygon_side,
+                     endpoint=False)
+    angle_dists = np.abs(angles[:,np.newaxis] - ls[np.newaxis,:])
+    angle_dists = np.where(angle_dists >= np.pi,
+                           2 * np.pi - angle_dists,
+                           angle_dists)
+    min_dist = np.argmin(angle_dists,axis=0)
+
+    bounding_polygon = [edge_y[min_dist],
+                        edge_x[min_dist]]
+
+    return bounding_polygon
 
 polygon_sides = [8,16,24,32]
 
@@ -15,72 +112,69 @@ for image_path in all_images:
     if os.path.isfile(mask_path):
         image_pairs[image_path] = mask_path
 
-hf = h5py.File(sys.argv[3], 'w')
-
 all_ls = {
     x:np.linspace(0, 2*np.pi, num=x,endpoint=False) for x in polygon_sides}
 
-for image_path in list(image_pairs.keys()):
-    print(image_path)
-    image = read_image(image_path)
-    mask = read_image(image_pairs[image_path])[:,:,0][:,:,np.newaxis]
-    mask = np.where(mask > 0,1,0)
-    edge_image = np.uint8(cv2.Laplacian(mask,cv2.CV_64F))
-    num_labels, labels_im = cv2.connectedComponents(mask)
-    bounding_boxes = []
-    bounding_polygons = {x:[] for x in all_ls}
-    centers = []
+with h5py.File(sys.argv[3], 'w') as hf:
 
-    g = hf.create_group(image_path.split(os.sep)[-1])
-    edge_group = g.create_group('edges')
+    for image_path in list(image_pairs.keys()):
+        print(image_path)
+        image = read_image(image_path)
+        mask = read_image(image_pairs[image_path])[:,:,0][:,:,np.newaxis]
+        edge_image = np.uint8(cv2.Laplacian(mask,cv2.CV_64F))
+        num_labels, labels_im = cv2.connectedComponents(mask)
+        bounding_boxes = []
+        bounding_polygons = {x:[] for x in all_ls}
+        centers = []
 
-    for c in range(1,num_labels):
-        edge_x,edge_y = np.where((labels_im == c) & (edge_image > 0))
-        object_edges = np.stack([edge_y,edge_x])
-        edge_group.create_dataset(str(c),
-                                  shape=object_edges.shape,
-                                  dtype=np.int32,
-                                  data=object_edges)
-        x,y = np.where(labels_im == c)
-        if len(x) < 10:
-            print('\tbigmad')
-        bbox = [np.min(x),np.min(y),
-                np.max(x),np.max(y)]
-        bbox_center = [int((bbox[0] + bbox[2])/2),
-                       int((bbox[1] + bbox[3])/2)]
-        center_x,center_y = bbox_center
-        bounding_boxes.append(bbox)
+        g = hf.create_group(image_path.split(os.sep)[-1])
+        edge_group = g.create_group('edges')
 
-        for polygon_side in all_ls:
-            ls = all_ls[polygon_side]
-            bounding_polygon = edges_to_polygon(edge_x-center_x,
-                                                edge_y-center_y,
-                                                polygon_side)
-            bounding_polygon[0] += center_y
-            bounding_polygon[1] += center_x
-            bounding_polygons[polygon_side].append(bounding_polygon)
-        centers.append([center_y,center_x])
+        for c in range(1,num_labels):
+            edge_x,edge_y = np.where((labels_im == c) & (edge_image > 0))
+            object_edges = np.stack([edge_y,edge_x])
+            edge_group.create_dataset(str(c),
+                                      shape=object_edges.shape,
+                                      dtype=np.int32,
+                                      data=object_edges)
+            x,y = np.where(labels_im == c)
+            if len(x) < 10:
+                print('\tbigmad')
+            bbox = [np.min(x),np.min(y),
+                    np.max(x),np.max(y)]
+            bbox_center = [int((bbox[0] + bbox[2])/2),
+                           int((bbox[1] + bbox[3])/2)]
+            center_x,center_y = bbox_center
+            bounding_boxes.append(bbox)
 
-    bounding_boxes = np.array(bounding_boxes)
-    bounding_polygons = {
-        x:np.array(bounding_polygons[x]) for x in bounding_polygons}
-    centers = np.array(centers)
-    #weight_map = get_weights(mask)[:,:,np.newaxis]
+            for polygon_side in all_ls:
+                ls = all_ls[polygon_side]
+                bounding_polygon = edges_to_polygon(edge_x-center_x,
+                                                    edge_y-center_y,
+                                                    polygon_side)
+                bounding_polygon[0] += center_y
+                bounding_polygon[1] += center_x
+                bounding_polygons[polygon_side].append(bounding_polygon)
+            centers.append([center_y,center_x])
 
-    g.create_dataset('image', shape=image.shape, dtype=np.uint8, data=image)
-    g.create_dataset('mask', shape=mask.shape, dtype=np.uint8, data=mask)
-    g.create_dataset('bounding_boxes',shape=bounding_boxes.shape,
-                     dtype=np.int16,data=bounding_boxes)
-    g.create_dataset('weight_map',shape=weight_map.shape,
-                     dtype=np.float32,data=weight_map)
-    bp = g.create_group('bounding_polygons')
-    for polygon_side in bounding_polygons:
-        bp.create_dataset(
-            str(polygon_side),
-            shape=bounding_polygons[polygon_side].shape,
-            dtype=np.int32,
-            data=np.int16(bounding_polygons[polygon_side]))
-    g.create_dataset('centers',shape=centers.shape,
-                     dtype=np.int32,data=np.int16(centers))
+        bounding_boxes = np.array(bounding_boxes)
+        bounding_polygons = {
+            x:np.array(bounding_polygons[x]) for x in bounding_polygons}
+        centers = np.array(centers)
+        weight_map = get_weights(mask)[:,:,np.newaxis]
 
-hf.close()
+        g.create_dataset('image', shape=image.shape, dtype=np.uint8, data=image)
+        g.create_dataset('mask', shape=mask.shape, dtype=np.uint8, data=mask)
+        g.create_dataset('bounding_boxes',shape=bounding_boxes.shape,
+                         dtype=np.int16,data=bounding_boxes)
+        g.create_dataset('weight_map',shape=weight_map.shape,
+                         dtype=np.float32,data=weight_map)
+        bp = g.create_group('bounding_polygons')
+        for polygon_side in bounding_polygons:
+            bp.create_dataset(
+                str(polygon_side),
+                shape=bounding_polygons[polygon_side].shape,
+                dtype=np.int32,
+                data=np.int16(bounding_polygons[polygon_side]))
+        g.create_dataset('centers',shape=centers.shape,
+                         dtype=np.int32,data=np.int16(centers))
